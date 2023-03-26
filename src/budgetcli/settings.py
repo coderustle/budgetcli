19d6515ed3bb8bc9ffe87f9e64e4616d1ebf4069
Madalin Popa <contact@madalinpopa.com>
2023-03-27 00:12:43 +0300

    Format style settings.py

--- src/budgetcli/settings.py
+++ src/budgetcli/settings.py
@@ -10,4 +10,3 @@ CONFIG_FILE_NAME = "config.json"
 USER_CONFIG_DIR = user_config_dir(APP_NAME, ensure_exists=True)
 CONFIG_FILE_PATH = os.path.join(USER_CONFIG_DIR, CONFIG_FILE_NAME)
 CREDENTIALS_SECRET_PATH = os.path.join(USER_CONFIG_DIR, "credentials.json")
-
