5c8c7a916cfb056da70c93452a5fa242b3826628
Madalin Popa <contact@madalinpopa.com>
2023-03-29 06:38:05 +0000

    Update auth command

--- src/budgetcli/commands/auth.py
+++ src/budgetcli/commands/auth.py
@@ -1,5 +1,5 @@
 """
-This module contains the command to authorize the user data in google. 
+This module contains the command to authorize the user data in google.
 """
 
 import typer
@@ -14,3 +14,7 @@ app = typer.Typer()
 def main() -> None:
     """Authorize the app to use the user data"""
     get_credentials()
+
+
+if __name__ == "__main__":
+    app()
