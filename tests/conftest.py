eb380b8dc67da1b64c57598714b6b4d40d002772
Madalin Popa <contact@madalinpopa.com>
2023-05-06 12:09:13 +0300

    Add more fixtures for transactions

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,51 @@ FIXTURES_FOLDER = Path(__file__).parent / "fixtures"
 
 
 @pytest.fixture
+def transactions_init_get_sheet():
+    file_path = FIXTURES_FOLDER / "get_transactions_sheet.json"
+
+    def json_response():
+        with file_path.open("rb") as f:
+            response = json.load(f)
+            return response
+
+    return json_response
+
+
+@pytest.fixture
+def transactions_update_response():
+    file_path = FIXTURES_FOLDER / "update_transactions.json"
+
+    def json_response():
+        with file_path.open("rb") as f:
+            response = json.load(f)
+            return response
+
+    return json_response
+
+
+@pytest.fixture
 def transactions_list_response():
     file_path = FIXTURES_FOLDER / "list_transactions.json"
-    with file_path.open("rb") as f:
-        response = json.load(f)
-        return response
+
+    def json_response():
+        with file_path.open("rb") as f:
+            response = json.load(f)
+            return response
+
+    return json_response
+
+
+@pytest.fixture
+def transactions_rows_response():
+    file_path = FIXTURES_FOLDER / "one_transaction.json"
+
+    def json_response():
+        with file_path.open("rb") as f:
+            response = json.load(f)
+            return response
+
+    return json_response
 
 
 @pytest.fixture
--- tests/unit/test_transactions.py
+++ tests/unit/test_transactions.py
@@ -11,14 +11,6 @@ def manager():
 
 
 @pytest.fixture
-def return_json_list(transactions_list_response):
-    def result():
-        return transactions_list_response
-
-    return result
-
-
-@pytest.fixture
 def fake_coro():
     async def coro():
         return None
@@ -40,49 +32,64 @@ def fake_sheet_coro():
     return mock_coro
 
 
-@pytest.fixture
-def transactions():
-    return [
-        ["05-05-2023", "salary", "", "200", "0"],
-        ["06-05-2023", "rent", "", "0", "100"],
-    ]
-
-
 @pytest.mark.asyncio
-async def test_init_sheet_exists(manager, fake_sheet_coro, fake_coro):
+async def test_init_sheet_exists(
+    manager, transactions_init_get_sheet, transactions_update_response
+):
     """Test init sheet get sheet"""
-    manager._update = MagicMock()
-    manager._update.return_value = fake_coro()
 
-    manager._get_sheet = MagicMock()
-    manager._get_sheet.return_value = fake_sheet_coro()
+    session_mock = AsyncMock()
+
+    get_response_mock = MagicMock()
+    get_response_mock.raise_for_status.return_value = None
+    get_response_mock.json = transactions_init_get_sheet
+
+    session_mock.get.return_value = get_response_mock
+
+    put_response_mock = MagicMock()
+    put_response_mock.raise_for_status.return_value = None
+    put_response_mock.json = transactions_update_response
+
+    session_mock.put.return_value = put_response_mock
+
+    manager.session = session_mock
 
     with patch("budgetcli.data_manager.update_config") as mock_config:
         await manager.init()
-        manager._get_sheet.assert_called_once()
         mock_config.assert_called_once()
-        manager._update.assert_called_once()
+        session_mock.get.assert_called_once()
+        session_mock.put.assert_called_once()
+        get_response_mock.raise_for_status.assert_called_once()
 
 
 @pytest.mark.asyncio
-async def test_init_sheet_create(manager, fake_sheet_coro, fake_coro):
+async def test_init_sheet_create(
+    manager, transactions_init_get_sheet, transactions_update_response
+):
     """Test init sheet create"""
 
-    manager._update = MagicMock()
-    manager._update.return_value = fake_coro()
+    session_mock = AsyncMock()
 
-    manager._get_sheet = MagicMock()
-    manager._get_sheet.return_value = fake_coro()
+    get_response_mock = MagicMock()
+    get_response_mock.raise_for_status.return_value = None
+    get_response_mock.json = transactions_init_get_sheet
 
-    manager._create_sheet = MagicMock()
-    manager._create_sheet.return_value = fake_sheet_coro()
+    session_mock.get.return_value = get_response_mock
+
+    put_response_mock = MagicMock()
+    put_response_mock.raise_for_status.return_value = None
+    put_response_mock.json = transactions_update_response
+
+    session_mock.put.return_value = put_response_mock
+
+    manager.session = session_mock
 
     with patch("budgetcli.data_manager.update_config") as mock_config:
         await manager.init()
-        manager._get_sheet.assert_called_once()
-        manager._create_sheet.assert_called_once()
         mock_config.assert_called_once()
-        manager._update.assert_called_once()
+        session_mock.get.assert_called_once()
+        session_mock.put.assert_called_once()
+        get_response_mock.raise_for_status.assert_called_once()
 
 
 @pytest.mark.asyncio
@@ -147,7 +154,7 @@ async def test_append_method_with_no_values(manager):
 
 
 @pytest.mark.asyncio
-async def test_get_records(manager, return_json_list):
+async def test_get_records(manager, transactions_list_response):
     """Test get transactions method"""
 
     # build url
@@ -156,7 +163,7 @@ async def test_get_records(manager, return_json_list):
 
     mock_response = MagicMock()
     mock_response.raise_for_status.return_value = None
-    mock_response.json = return_json_list
+    mock_response.json = transactions_list_response
 
     session_mock = AsyncMock()
     session_mock.get.return_value = mock_response
@@ -164,20 +171,34 @@ async def test_get_records(manager, return_json_list):
     manager.session = session_mock
 
     result = await manager.get_records()
-    
+
     session_mock.get.assert_called_once_with(url)
     mock_response.raise_for_status.assert_called_once()
     assert result
+    assert len(result) == 100
 
 
 @pytest.mark.asyncio
-async def test_get_records_rows_option(manager, transactions):
+async def test_get_records_rows_option(manager, transactions_rows_response):
     """Test get transactions with rows option"""
-    expected = [["2023-04-05", "category", " ", "20", "0"]]
-    with patch.object(AbstractDataManager, "_list") as mock_list:
-        mock_list.return_value = expected
-        result = await manager.get_records(rows=1)
-        assert len(result) == 1
+
+    # build url
+    params = "majorDimension=ROWS"
+    url = f"{manager.base_url}/values/TRANSACTIONS!A2:E2?{params}"
+
+    response_mock = MagicMock()
+    response_mock.raise_for_status.return_value = None
+    response_mock.json = transactions_rows_response
+
+    session_mock = AsyncMock()
+    session_mock.get.return_value = response_mock
+
+    manager.session = session_mock
+
+    result = await manager.get_records(rows=1)
+    session_mock.get.assert_called_once_with(url)
+    response_mock.raise_for_status.assert_called_once()
+    assert len(result) == 1
 
 
 # @pytest.mark.skip
